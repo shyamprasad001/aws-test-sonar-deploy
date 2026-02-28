1d2d25242e3ebced1237ac636c72bba35b5937ee
Shyam Mantri <iamshyam1911@gmail.com>
2026-02-28 11:38:55 +0530

    first commit

---
+++ src/test/java/com/example/demo/CsecontrollerTest.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsecontrollerTest {
+	@Autowired
+	Csecontroller c;
+	@Test
+	void csetest() {
+		int result = c.addcse(2,3);
+		assertEquals(6,result);
+	}
+	
+	
+
+}
